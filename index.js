1ab84fa7d79f956875a804be807de1f9cbcd8dbc
xuanphong <phongpx1509@gmail.com>
2024-06-03 20:43:56 +0700

    Đã thêm tính năng mới

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Biến Varial 
 let first_Name = 'phong'
 let day = 15
 let month = 9
@@ -5,3 +6,21 @@ let  year = 2002
 let  hometwone = "Nghe An"
 
 console.log(first_Name, day, month , year, hometwone );
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows [2] = 10;
+// console.log(rows[2])
+rows.push("freeCodeCamp"); 
+
+let popped = rows.pop(); 
+console.log(rows);
+console.log(popped);
+
+document.write(rows)
+document.write(popped);
+// window.prompt("Enter name")
+
+
